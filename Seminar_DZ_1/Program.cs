13a0fd86b451344c8ada0fef1f7fb7bd501096ba
Evgeny.A <akimoveo@gmail.com>
2022-12-18 14:50:00 +0400

    задача 2

---
+++ Seminar_DZ_1/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Clear();
+Console.WriteLine("Hello, Рустем!");
+Console.WriteLine();
+
+// Задача 2: Напишите программу, которая на вход принимает 
+//два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.Write("вввести 2 числа и определить, какое число из них большее, а какое меньшее");
+Console.WriteLine();
+
+Console.Write("Введите первое число: a ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: b ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a < b)
+    Console.WriteLine($"a меньше b, b {b} - max");
+else
+    Console.WriteLine($"a больше b, а {a} - max");
+
+
