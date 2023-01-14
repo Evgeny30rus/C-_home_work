5a9486cc8ce8704a8295db6ec427ca70bf39f641
Evgeny.A <akimoveo@gmail.com>
2023-01-14 21:30:07 +0400

    Семинар 4 продолжение

---
+++ Seminar_DZ_4/Program.cs
@@ -0,0 +1,69 @@
+﻿Console.Clear();
+Console.WriteLine("Hello, Рустем!");
+Console.WriteLine();
+
+// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и 
+//            возводит число A в натуральную степень B.
+//            3, 5 -> 243 (3⁵)
+//            2, 4 -> 16
+Console.Write("Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.");
+Console.WriteLine();
+
+Console.Write("ВВедите число А");
+Console.WriteLine();
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("ВВедите число B");
+Console.WriteLine();
+int b = Convert.ToInt32(Console.ReadLine());
+int result = a;
+int i = 2;
+
+while (i<=b)
+{
+    result = result*a;
+    i++;
+}
+
+Console.WriteLine();
+Console.WriteLine("результат:");
+Console.WriteLine(result);
+Console.WriteLine();
+Console.WriteLine("конец алгоритма");
+
+
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+Console.WriteLine();
+Console.Write("Задача 27: Напишите программу, которая принимает на вход число N и выдаёт сумму цифр в числе.");
+Console.WriteLine();
+
+Console.Write("ВВедите число N");
+Console.WriteLine();
+
+int n = Convert.ToInt32(Console.ReadLine());
+int res = 0;
+
+while (i)
+{
+    int n
+
+
+    i++;
+}
+
+
+
+
+Console.WriteLine(res);
+
+
+
+Console.Write("Результат действия");
+Console.WriteLine();
+Console.Write(count);
+
+Console.WriteLine();
+Console.WriteLine("конец алгоритма");
