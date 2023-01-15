1be11655f4c1d5c5589b46ddbab62494c3b24039
Evgeny.A <akimoveo@gmail.com>
2023-01-15 17:08:38 +0400

    Задача 34 и 38 офоромлены

--- Seminar_DZ_5/Program.cs
+++ Seminar_DZ_5/Program.cs
@@ -6,34 +6,34 @@ Console.WriteLine();
 // Напишите программу, которая покажет количество чётных чисел в массиве.
 // [345, 897, 568, 234] -> 2
 
-// Console.Write("Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами.Напишите программу, которая покажет количество чётных чисел в массиве.");
-// Console.WriteLine();
+Console.Write("Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами.Напишите программу, которая покажет количество чётных чисел в массиве.");
+Console.WriteLine();
 
-// void input_array(int [] array)          // создание и заполнение массива рандомом
-// {
-//     for (int i = 0; i < array.Length; i++)
-//         array[i] = new Random().Next(100, 999); // [100;999]
-// }
-// int f = 2;
-// int release(int[] array)
-// {
-//     int count = 0;
-//     for (int i = 0; i < array.Length; i++)
-//         if (array[i] % f == 0)
-//                 count ++;
-//     return count;
-// }
-// // Console.Clear();
-// Console.Write("Введите кол-во элементов массива: ");
-// int n = Convert.ToInt32(Console.ReadLine());
-// int[] array = new int[n];
-// input_array(array);
-// Console.WriteLine($"[{string.Join(", ", array)}]");
+void input_array(int [] array)          // создание и заполнение массива рандомом
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(100, 999); // [100;999]
+}
+int f = 2;
+int release(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+        if (array[i] % f == 0)
+                count ++;
+    return count;
+}
+// Console.Clear();
+Console.Write("Введите кол-во элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+input_array(array);
+Console.WriteLine($"[{string.Join(", ", array)}]");
 
-// Console.WriteLine(release(array));
+Console.WriteLine(release(array));
 
-// Console.WriteLine();
-// Console.WriteLine("конец алгоритма 34");
+Console.WriteLine();
+Console.WriteLine("конец алгоритма 34");
 
 // Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, 
 // стоящих на нечётных позициях.
@@ -75,47 +75,49 @@ Console.WriteLine();
 // Console.WriteLine();
 // Console.WriteLine("конец алгоритма 36");
 
-Console.Write("ошибка");
+Console.Write("задача 36 ошибка");
 
 // Задача 38: Задайте массив чисел. Найдите разницу между максимальным и минимальным элементов массива.
 // [3 7 22 2 78] -> 76
+Console.WriteLine();
 Console.WriteLine();
 
 Console.Write("Задача 38: Задайте массив чисел. Найдите разницу между максимальным и минимальным элементов массива");
 Console.WriteLine();
 
-void input_array(int [] array)
+void input_array2(int [] array2)
 {
-    for (int i = 0; i < array.Length; i++)
-        array[i] = new Random().Next(0, 100); // [0;100]
+    for (int i = 0; i < array2.Length; i++)
+        array2[i] = new Random().Next(0, 100); // [0;100]
 }
 
 int result=0;
 int max=0; 
 int min=0;
-void release(int[] array)
+void release2(int[] array2)
 {
-    int max = array[0], min = array[0];
-        for (int i = 0; i < array.Length; i++)
-            if (array[i] >= max) 
-            max = array[i] ;
+    int max = array2[0], min = array2[0];
+        for (int i = 0; i < array2.Length; i++)
+            if (array2[i] >= max) 
+            max = array2[i] ;
         else
-            if (array[i] <= min)
-            min = array[i];
+            if (array2[i] <= min)
+            min = array2[i];
     Console.WriteLine($"max{max}  min{min}");
 
     result = max - min;
 }
 
 
-Console.Clear();
+//Console.Clear();
 Console.Write("Введите кол-во элементов: ");
-int n = Convert.ToInt32(Console.ReadLine());
-int[] array = new int[n];
-input_array(array);
-Console.WriteLine($"[{string.Join(", ", array)}]");
-release(array);
+int n2 = Convert.ToInt32(Console.ReadLine());
+int[] array2 = new int[n2];
+input_array2(array2);
+Console.WriteLine($"[{string.Join(", ", array2)}]");
+release2(array2);
 Console.WriteLine(result);
 
 Console.WriteLine();
 Console.WriteLine("конец алгоритма 38");
+Console.WriteLine();
