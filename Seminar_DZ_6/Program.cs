c91122d4b4d650a4c8266dcd5cbd16562da4d96d
Evgeny.A <akimoveo@gmail.com>
2023-01-28 20:10:42 +0400

    Начало выполнения задания 41

---
+++ Seminar_DZ_6/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -
+// 1, -7, 567, 89, 223-> 3
+Console.WriteLine("Hello, Рустем!");
+Console.WriteLine();
+
+
+
+
+
+
+Console.WriteLine();
+Console.WriteLine("конец алгоритма 41");
