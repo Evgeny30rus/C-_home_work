01b942e24458af411964668cc515c0911ab275b0
Evgeny.A <akimoveo@gmail.com>
2023-01-28 21:56:24 +0400

    задача 41 решена

--- Seminar_DZ_6/Program.cs
+++ Seminar_DZ_6/Program.cs
@@ -1,17 +1,65 @@
 ﻿// See https://aka.ms/new-console-template for more information
+Console.Clear();
 Console.WriteLine("Hello, World!");
 
 
 // Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
 // 0, 7, 8, -2, -2 -
 // 1, -7, 567, 89, 223-> 3
+
 Console.WriteLine("Hello, Рустем!");
 Console.WriteLine();
 
+Console.WriteLine();
+Console.Write("Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.");
+Console.WriteLine();  
+Console.WriteLine("ВВедите число М");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
 
+void input_array(int [] array)           //  сознательно ушел от ввода чисел в массив с клавиатуре как в задании, посчитал это неудобным для проверки... Если примите за ошибку могу переделать
+{
+    for (int i = 0; i < array.Length; i++)
+        array [i] = new Random().Next(-m, m); // [-m;M]       
+}
 
+int release(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+        if (array[i] > 0)
+                count ++;
+    return count;
+}
 
+int[] array = new int[m];
+input_array(array);
+Console.WriteLine($"[{string.Join(", ", array)}]");
 
+Console.WriteLine(release(array));
 
 Console.WriteLine();
 Console.WriteLine("конец алгоритма 41");
+
+// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.WriteLine();
+Console.Write("Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых");
+Console.WriteLine();  
+Console.WriteLine("ВВедите число b1");
+int b1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine();
+Console.WriteLine("ВВедите число b2");
+int b2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine();
+Console.WriteLine("ВВедите число k1");
+int k1 = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine();
+Console.WriteLine("ВВедите число k2");
+int k2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+
