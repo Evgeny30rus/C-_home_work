c9b88848a09829a438268ce665c3cbbfed49d246
Evgeny.A <akimoveo@gmail.com>
2023-01-29 22:09:36 +0400

    ДЗ 9 рекурсия

--- SEMINAR_WORK/Program.cs
+++ SEMINAR_WORK/Program.cs
@@ -549,3 +549,119 @@ Console.WriteLine("Hello, World!");
 // Console.WriteLine("Конечный массив");
 // Release(matrix);
 
+//  РЕКУРСИЯ
+
+// Написать программу, которая складывает два числа
+
+// int sum(int a, int b)
+// {
+//     if (b == 0)
+//         return a;
+//     return sum(a + 1, b - 1);
+// }
+
+// Console.Clear();
+// Console.Write("Введите первое число: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите второе число: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine(sum(n, m));
+
+// Задача 63: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+// N = 5 -> "1, 2, 3, 4, 5"
+// N = 6 -> "1, 2, 3, 4, 5, 6"
+
+// string rec(int n)
+// {
+//      if (n == 1)
+//         return "1";
+//     return rec(n - 1) + $" {n}";
+// }
+
+// Console.Clear();
+// Console.Write("Введите первое число: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine(rec(n));
+
+// Задача 65: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5 -> "1, 2, 3, 4, 5"
+// M = 4; N = 8 -> "4, 6, 7, 8"
+
+// string rec(int m, int n)
+// {
+//      if (n == m)
+//         return $"{n}";
+//     return rec(m,n - 1) + $" {n}";
+// }
+
+// Console.Clear();
+// Console.Write("Введите первое число: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите второе число: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine(rec(n, m));
+
+// Задача 67: Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+// int rec(int n)
+// {
+//     if (n < 10)
+//         return n;
+//     return rec(n / 10) + n % 10;
+// }
+
+// Console.Clear();
+// Console.Write("Введите первое число: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine(rec(n));
+
+// // Задача 69: Напишите программу, которая на вход принимает два числа A и B, и возводит число А в целую степень 
+//                            B с помощью рекурсии.
+// // A = 3; B = 5 -> 243 (3⁵)
+// // A = 2; B = 3 -> 8
+// // 69
+// int rec(int a, int b)
+// {
+//     if (b == 0)
+//         return 1;
+//     return rec(a, b - 1) * a;
+// }
+
+// Console.Clear();
+// Console.Write("Введите первое число: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите первое число: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine(rec(a, b));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Seminar_DZ_9/Program.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Clear();
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, Рустем!");
+//      ЧЕРЕЗ РЕКУРСИЮ
+// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов 
+//            в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+Console.WriteLine();
+
+Console.Write("Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.");
+Console.WriteLine();
+
+
+int rec(int m, int n)
+{
+    if (n == m)
+        return 0;
+    return rec(m,n - 1) + n + m;
+}
+
+Console.Clear();
+Console.Write("Введите первое число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(rec(m, n));
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+//            Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+
